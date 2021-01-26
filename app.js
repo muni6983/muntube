8111abaa9951946bc623e1093b97b0223c979a52
gyeongmun <bada6983@gmail.com>
2021-01-26 22:03:20 +0900

    finish 2.16

--- app.js
+++ app.js
@@ -8,20 +8,22 @@ import cookeParser from "cookie-parser";
 import bodyParser from "body-parser";
 //express middleware -body로 부터 정보를 얻을 수 있게 해주는 것?
 //서버가 유저로부터 받은 데이터를 이해하는 방법???
+import { localsMiddleware } from "./middlewares";
 import userRouter from "./routers/userRouter";
 import videoRouter from "./routers/videoRouter";
 import globalRouter from "./routers/globalRouter";
 import routes from "./routes";
-
 const app = express();
 
+app.use(helmet());
 app.set("view engine", "pug");
 app.use(cookeParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 //지금은 뭐하는건지 잘 모르겠지만 나중에 앱을 만들다가 여기와서 이걸 지워보면 이게 어떤 애들인지 알 수 있을거야
-app.use(helmet());
 app.use(morgan("dev"));
+
+app.use(localsMiddleware);
 
 app.use(routes.home, globalRouter);
 app.use(routes.users, userRouter);
